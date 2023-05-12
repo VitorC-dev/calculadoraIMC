801d6bd826a73cad6d05d3ab4f8d1fa81e7660ed
vitor <vitorcruz236@hmotmail.com>
2023-05-11 21:57:50 -0300

    Add os arquivos HTML, JS, CSS e Images

---
+++ assets/JS/script.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('form');
+form.addEventListener('submit', function(event) {
+    /* Previne o comportamento padrão do evento submit do JS, ou seja,
+    impede o recarregamento da página */
+    event.preventDefault();
+
+    const weight = document.getElementById('weight').value;
+    const height = document.getElementById('height').value;
+
+    const bmi = (weight / (height * height)).toFixed(2);
+
+    if (!isNaN(bmi)) {
+        const value = document.getElementById('value');
+        let description = '';
+
+        value.classList.add('attention');
+        
+        document.getElementById('infos').classList.remove('hidden');
+    
+        if (bmi < 18.5){
+            description = 'Cuidado! Você está abaixo do peso!';
+        } 
+
+        else if (bmi >= 18.5 && bmi <= 25) {
+            description = "Você está no peso ideal!";
+            value.classList.remove('attention');
+            value.classList.add('normal');
+        }
+
+        else if (bmi > 25 && bmi <= 30) {
+            description = "Cuidado! Você está acima do peso!";
+        }
+
+        else if (bmi > 30 && bmi <= 35) {
+            description = "CUIDADO! Você está com OBESIDADE NV.I!";
+        }
+
+        else if (bmi > 35 && bmi <= 40) {
+            description = "CUIDADO! Você está com OBESIDADE NV.II!";
+        }
+
+        else {
+            description = "CUIDADO! Você está com OBESIDADE NV.III!";
+        }
+
+        value.textContent = bmi.replace('.', ',');
+        document.getElementById('description').textContent = description;
+    }
+});
+
+form.addEventListener('keypress', function(event) {
+    if (event.key === 'press') {
+        document.getElementById('#calculate').click();
+    }
+});
